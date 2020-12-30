21a8095fd680402544a43c08c66a1f0fa99a49be
Daksh Kulshreshtha <akshd@outlook.com>
2020-12-30 22:10:38 +0530

    UI Fixes + Change username button

--- src/components/OpenSource/OpenSource.js
+++ src/components/OpenSource/OpenSource.js
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from 'react';
 
 import axios from 'axios';
 import { ListGroup, ListGroupItem, Spinner } from 'reactstrap';
-import { github as user } from '../../data.json';
+import { github as username } from '../../data.json';
 
 import './OpenSource.scss';
 
 export default function OpenSource() {
+    const [user, setUser] = useState(username)
     const [repos, setRepos] = useState([])
     const baseURL = `https://api.github.com/users/` + user + `/repos`;
 
@@ -21,28 +22,42 @@ export default function OpenSource() {
 
     if (repos) {
         return (
-            <div className="openSource">
-                {
-                    repos.map(el => (
-                        <div key={el.id}>
-                            <ListGroup>
-                                <ListGroupItem className="item">
-                                    <span className="repoName"><a href={el.html_url}>{el.full_name}</a></span>
-                                    <br />
-                                    <span>- {el.description}</span>
-                                    <br />
-                                    <span className="language">🟡 {`${el.language}`}</span>
-                                </ListGroupItem>
-                            </ListGroup>
-                        </div>
-                    ))
-                }
-            </div>
+            <>
+                <center>
+                    <form>
+                        <input type="text" placeholder="Enter a github username" onChange={(e) => setUser(e.target.value)} />
+                        <button onClick={(e) => {
+                            e.preventDefault()
+                            setRepos(undefined)
+                            getData()
+                        }}>Update</button>
+                    </form>
+                </center>
+
+                <div className="openSource">
+
+                    {
+                        repos.map(el => (
+                            <div key={el.id}>
+                                <ListGroup>
+                                    <ListGroupItem className="item">
+                                        <span className="repoName"><a href={el.html_url}>{el.full_name}</a></span>
+                                        <br />
+                                        <span>- {el.description}</span>
+                                        <br />
+                                        <span className="language">🟡 {`${el.language}`}</span>
+                                    </ListGroupItem>
+                                </ListGroup>
+                            </div>
+                        ))
+                    }
+                </div>
+            </>
         )
     } else {
         return (
             <>
-                <Spinner color="primary"></Spinner>
+                <center><Spinner color="primary"></Spinner></center>
             </>
         )
     }
