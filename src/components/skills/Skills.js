22fefc89e97df54ddabc37556f45f63138c039ff
pushparaja <appumoorthy15@gmail.com>
2021-01-24 15:25:22 +0000

    skill cards have implemented with scroll

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -3,12 +3,16 @@ import React from 'react';
 import './Skills.scss';
 
 
- const Skills = ()=> {
+const Skills = () => {
+
+    const onMouseHoverHandler = (event) => {
+        event.target.nextSibling ? event.target.nextSibling.scroll({ left: 0, top: 500, behavior: 'smooth' }) : null;
+    }
 
     let skillData = [
         {
             name: "Frontend",
-            list: ["Html", "Css", "Scss", "JavaScript", "React", "Angular", "Webpack"]
+            list: ["Html", "Css", "Scss", "JavaScript", "vue js", "React", "Angular", "Webpack"]
         },
         {
             name: "Backend",
@@ -16,37 +20,39 @@ import './Skills.scss';
         },
         {
             name: "Database",
-            list: ["Postgress", "SQL", "Mongodb"]
+            list: ["Postgress", "SQL", "Mongodb", "MySQL", "Graphql"]
         },
     ]
-    
+
     //  const {frontend,backend,database} = useSelector((state) => state.SkillsReducer)
     return (
-            <section className="skill">
-                <div className="skill__container">
-                    <div className="skill__row">
-                        {
-                            skillData.map(data => (
-                                <div className="skill__card">
-                                    <div className="skill__card-header">
-                                        <h3 className="skill__primary-heading">{data.name}</h3>
-                                    </div>
-                                    <div className="skill__card-body">
-                                        <ul className="skill__list">
-                                            {
-                                                data.list.map(skill => (
-                                                    <li className="skill__item"><p className="skill__text">{skill}</p></li>
-                                                ))
-                                            }
-                                        </ul>
-                                    </div>
+        <section className="skill">
+            <div className="skill__container">
+                <div className="skill__row">
+                    {
+                        skillData.map((data, index) => (
+                            <div className="skill__card">
+                                <div className="skill__card-header" onMouseOver={onMouseHoverHandler}>
+                                    <h3 className="skill__primary-heading" >{data.name}</h3>
                                 </div>
-                            ))
-                        }
-                    </div>
+                                <div className="skill__card-body" >
+                                    <ul className="skill__list" id={"list__id" + index} >
+                                        {
+                                            data.list.map((skill, skillIndex) => (
+                                                <li className="skill__item" id={"item" + skillIndex}><p className="skill__text">{skill}</p></li>
+                                            ))
+                                        }
+                                    </ul>
+
+                                </div>
+
+                            </div>
+                        ))
+                    }
                 </div>
-            </section>
+            </div>
+        </section>
     );
 }
 
-export default Skills ;
+export default Skills;
